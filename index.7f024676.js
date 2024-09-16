73215a0d364a9694f684729b8a58e821bb7352f6
Mariia <maria.gula0307@gmail.com>
2024-09-16 13:39:13 +0300

    make build

---
+++ index.7f024676.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.clientWidth,l=e.clientHeight,i=document.querySelector(".spider"),c=i.clientWidth,n=i.clientHeight;i.style.top=l/2-n/2+"px",i.style.left=t/2-c/2+"px";
+//# sourceMappingURL=index.7f024676.js.map
